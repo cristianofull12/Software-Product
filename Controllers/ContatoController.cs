bf3ba3ac86d3af05e5e5d649a520d69171903e57
cristianofull12 <seu-email-no-github>
2024-11-07 13:24:43 -0300

    AC3 - Visualizar detalhes de um registro no banco de dados

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -55,5 +55,14 @@ namespace SoftwareProduct.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Detalhes(int id){
+            var contato = _context.Contatos.Find(id);
+
+            if (contato == null)
+                return RedirectToAction(nameof(Index));
+
+            return View(contato);
+        }
     }
 }
